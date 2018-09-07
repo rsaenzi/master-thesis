89aa3f10a290c12897b5b714c4e217d15aad9cbe
Rigoberto Saenz Imbacuan <beto456789@gmail.com>
2018-09-07 03:49:08 -0500

    Fighter Ambience audio added Sun flares added Missile Sidewinder added

--- UnityProject/AerialWarfareSimulator/Assets/Scripts/FighterSimpleMotion.cs
+++ UnityProject/AerialWarfareSimulator/Assets/Scripts/FighterSimpleMotion.cs
@@ -4,42 +4,78 @@ using UnityEngine;
 
 public class FighterSimpleMotion : MonoBehaviour {
 
+    // Components
+    private Rigidbody body;
+
     // Motion
-    private float verticalSpeed = 18;
-    private float horizontalSpeed = 15;
-    private float forceValue = 25;
-    private ForceMode mode = ForceMode.Acceleration;
+    private readonly ForceMode mode = ForceMode.Acceleration;
+    private readonly float motionForceUp = 20;
+    private readonly float motionForceDown = 30;
+    private readonly float motionForceLeft = 30;
+    private readonly float motionForceRight = 30;
+
+    // Motion Targets
+    private Vector3 motionTargetUp;
+    private Vector3 motionTargetDown;
+    private Vector3 motionTargetLeft;
+    private Vector3 motionTargetRight;
 
     // Rotation
-    private float rotationSpeed = 1f;
-    private float horizontalRotation = 45;
-    private float verticalRotation = 45;
     private Vector3 rotationVector = Vector3.zero;
+    private readonly float interpolationSpeed = 1.2f;
+    private readonly float rotationSpeedUp = 50;
+    private readonly float rotationSpeedDown = 45;
+    private readonly float rotationSpeedLeft = 45;
+    private readonly float rotationSpeedRight = 45;
 
-	void Update () {
+    // Rotation Targets
+    private Vector3 rotationTargetUp;
+    private Vector3 rotationTargetDown;
+    private Vector3 rotationTargetLeft;
+    private Vector3 rotationTargetRight;
+
+    void Awake() {
+
+        // Components
+        body = GetComponent<Rigidbody>();
+
+        // Motion Targets
+        motionTargetUp = Vector3.up * motionForceUp;
+        motionTargetDown = Vector3.down * motionForceDown;
+        motionTargetLeft = Vector3.left * motionForceLeft;
+        motionTargetRight = Vector3.right * motionForceRight;
+
+        // Rotation Targets
+        rotationTargetUp = new Vector3(-rotationSpeedUp, 0, 0);
+        rotationTargetDown = new Vector3(rotationSpeedDown, 0, 0);
+        rotationTargetLeft = new Vector3(0, 0, rotationSpeedLeft); ;
+        rotationTargetRight = new Vector3(0, 0, -rotationSpeedRight);
+    }
+
+    void Update() {
 
         rotationVector = Vector3.zero;
 
-        if (Input.GetKey(KeyCode.UpArrow)) {
-            this.GetComponent<Rigidbody>().AddForce(Vector3.up * forceValue, mode);
-            rotationVector += new Vector3(-verticalRotation, 0, 0);
+        if(Input.GetKey(KeyCode.UpArrow)) {
+            body.AddForce(motionTargetUp, mode);
+            rotationVector += rotationTargetUp;
         }
 
-        if (Input.GetKey(KeyCode.DownArrow)) {
-            this.GetComponent<Rigidbody>().AddForce(Vector3.down * forceValue, mode);
-            rotationVector += new Vector3(verticalRotation, 0, 0);
+        if(Input.GetKey(KeyCode.DownArrow)) {
+            body.AddForce(motionTargetDown, mode);
+            rotationVector += rotationTargetDown;
         }
 
-        if (Input.GetKey(KeyCode.LeftArrow)){
-            this.GetComponent<Rigidbody>().AddForce(Vector3.left * forceValue, mode);
-            rotationVector += new Vector3(0, 0, horizontalRotation);
+        if(Input.GetKey(KeyCode.LeftArrow)) {
+            body.AddForce(motionTargetLeft, mode);
+            rotationVector += rotationTargetLeft;
         }
 
-        if (Input.GetKey(KeyCode.RightArrow)) {
-            this.GetComponent<Rigidbody>().AddForce(Vector3.right * forceValue, mode);
-            rotationVector += new Vector3(0, 0, -horizontalRotation);
+        if(Input.GetKey(KeyCode.RightArrow)) {
+            body.AddForce(motionTargetRight, mode);
+            rotationVector += rotationTargetRight;
         }
 
-        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.Euler(rotationVector), rotationSpeed * Time.deltaTime);
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(rotationVector), interpolationSpeed * Time.deltaTime);
     }
 }
