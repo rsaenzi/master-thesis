489a8e9bbe6b27eccdf250de25f6b6b81bc3736a
Rigoberto Saenz <beto456789@gmail.com>
2020-05-31 20:48:20 -0500

    First executable exported

---
+++ SoccerExperiments/PlaySoccer.sh
@@ -0,0 +1,51 @@
+#! /usr/bin/env bash
+
+# To make this file executable run 'chmod +x PlaySoccer.sh'
+# To run this file open a terminal and run './PlaySoccer.sh'
+
+clear
+
+echo
+echo '-------------------------------------------------------------------------------------------------------------'
+echo
+echo 'Evaluating the impact of Curriculum Learning on the training process for an intelligent agent in a video game'
+echo 'Maestría en Ingeniería de Sistemas y Computación, Modalidad Profundización'
+echo 'Universidad Nacional de Colombia - Sede Bogotá'
+echo
+echo 'Supervised by:'
+echo 'PhD. Jorge Eliecer Camargo Mendoza'
+echo 'jecamargom@unal.edu.co'
+echo
+echo 'Presented by:'
+echo 'Rigoberto Sáenz Imbacuán'
+echo 'rsaenzi@unal.edu.co'
+echo
+echo '-------------------------------------------------------------------------------------------------------------'
+echo
+
+echo
+echo 'Current Directory:'
+cd '/Users/rsaenz/Documents/Projects/master-thesis/SoccerExperiments'
+pwd
+ls
+
+echo
+echo 'Adding mlagents to $PATH:'
+export PATH=$PATH:/Users/rsaenz/Library/Python/3.7/bin
+echo $PATH
+
+echo
+echo 'Checking Python version:'
+which -a python
+python3 --version
+python3 -m pip -V
+
+echo
+echo 'Running ML-Agents:'
+#mlagents-learn -h
+mlagents-learn config/trainer_config.yaml --env SoccerAcademy --force --run-id match_0 --save-freq 10000 --num-envs 1
+
+echo
+echo 'Opening TensorBoard:'
+open http://localhost:6006/
+tensorboard --logdir=summaries
