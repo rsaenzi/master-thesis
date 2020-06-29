25cff8471d9a203fda939f3f4743cfb8841a9418
Rigoberto Saenz <beto456789@gmail.com>
2020-06-29 03:07:38 -0500

    non-working commands removed from .sh for google compute engine

--- SoccerExperiments/StartTrainingOnGCE.sh
+++ SoccerExperiments/StartTrainingOnGCE.sh
@@ -38,6 +38,6 @@ mlagents-learn -h
 echo 'Running 100M experiments with Proximal Policy Optimization (PPO) + Curricula A:'
 mlagents-learn 'master-thesis/SoccerExperiments/TrainingConfigPPO.yaml' --env='master-thesis/SoccerExperiments/builds/linux/Linux_v11_4fields.x86_64' --run-id=PPO_CurriculaA --num-envs 1 --curriculum 'master-thesis/SoccerExperiments/TrainingCurriculaA.yaml' --no-graphics --force
 
-# Get experiments (run on other terminal windows that has not a ssh open connection)
-gcloud compute scp --recurse instance-for-ml-agents:/home/rsaenz/models /Users/rsaenz/Desktop/Experiments_PPO_CurriculaA
-gcloud compute scp --recurse instance-for-ml-agents:/home/rsaenz/summaries /Users/rsaenz/Desktop/Experiments_PPO_CurriculaA
+# Shutdown the instance to avoid more charges
+sleep 30
+sudo shutdown -h now
